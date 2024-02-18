053084df30f0b855e663b8559ecfd4f57f479f32
Janickraherii <55908684+Janickraherii@users.noreply.github.com>
2024-02-18 12:30:20 +0100

    Formulaire contact v1

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,13 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Accueil';
 import React, { useState } from 'react';
+import ContactUsForm from './pages/Contact_form';
 
 function App() {
   const [backgroundColor] = useState('#111111'); 
   return (
     <div style={{backgroundColor}}>
-      <Header/>
-      <Home/>
-      <Footer/>
+      <ContactUsForm/>
     </div>
   );
 }
