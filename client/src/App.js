3a095c8c6d470ff9f96e3ff84a685ba4bbb3bce7
Janickraherii <55908684+Janickraherii@users.noreply.github.com>
2024-02-23 17:26:35 +0100

    front-end v1

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import Header from './components/Header';
 import Home from './pages/Accueil';
 import React, { useState } from 'react';
 import ContactUsForm from './pages/Contact_form';
+import AuthForm from './pages/Auth_form';
 
 function App() {
   const [backgroundColor] = useState('#111111'); 
   return (
     <div style={{backgroundColor}}>
-      <ContactUsForm/>
+      <AuthForm/>
     </div>
   );
 }
---
+++ client/src/pages/Auth_form.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import logo from '../assets/logo.png'
+import image from '../assets/dog_image.png'; 
+import facebook from '../assets/logo-facebook.png';
+import gmail from '../assets/logo-gmail.svg';
+import line from '../assets/line.svg';
+
+function AuthForm() {
+
+  return (
+    <>
+    <div className='flex flex-row justify-end space-x-40'>
+        <div className='min-h-screen '>
+            <div className='mt-48'>
+                <img src={logo} alt="image1" className='w-44 ml-6 mb-0' />
+                <p className='text-xs text-[#7BE3DB] mb-6'>Libérez l'humour numérique sans limites !</p> 
+                    <div className='flex flex-col space-y-4'>
+                        <button className='bg-[#DA51A0] rounded-3xl text-white text-xs py-3'> Se connecter </button>
+                          <img src={line} alt="image1" className='w-56 mb-0' />
+                        <button className='flex flex-row justify-center items-center bg-[#FFFFFF] rounded-3xl text-xs py-3 px-6'> Se connecter avec 
+                          <img src={gmail} className='ml-3' alt="logo-gmail" /></button>
+                        <button className=' flex flex-row justify-center items-center bg-[#515CDA] rounded-3xl text-white  text-xs py-2 px-2'> Se connecter avec 
+                        <img src={facebook} className='ml-3' alt='logo-facebook' /></button>
+                          
+                    </div>
+            </div>
+        </div>
+        <div>
+        <img src={image} alt="image1" />             
+        </div>
+    </div>
+    
+
+   
+    
+
+    </> 
+  
+  );
+}
+
+export default AuthForm;
