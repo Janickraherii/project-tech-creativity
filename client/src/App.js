6010eacc1d90efd843844468907f0e288212ee86
Janickraherii <55908684+Janickraherii@users.noreply.github.com>
2024-02-25 12:47:47 +0100

    Dropzone

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,13 @@ import Home from './pages/Accueil';
 import React, { useState } from 'react';
 import ContactUsForm from './pages/Contact_form';
 import AuthForm from './pages/Auth_form';
+import Dropzone from './pages/Dropzone';
 
 function App() {
   const [backgroundColor] = useState('#111111'); 
   return (
     <div style={{backgroundColor}}>
-      <AuthForm/>
+      <Dropzone/>
     </div>
   );
 }
