854816db5b7611b6fe792b0696864385641db99a
Janickraherii <55908684+Janickraherii@users.noreply.github.com>
2024-03-20 11:23:33 +0100

    add pdp to profile

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Accueil';
 // import Actuality from './pages/Actuality';
-// import Profile from './pages/Profile';
+import Profile from './pages/Profile';
 import Dropzone from './pages/Dropzone';
 import AuthForm from './pages/AuthForm';
 import SignUp from './pages/SignUp';
 import Modification from './pages/Modification';
 import UseApi from './pages/UseApi';
 import Stack from './pages/Stack';
-
-// const UseApiWithStack = () => {
-//   return (
-//       <UseApi>
-//           <Stack />
-//       </UseApi>
-//   );
-// };
 
 function App() {
     return (
@@ -25,7 +17,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           {/* <Route exact path="/actualites" element={<Actuality />} /> */}
-          {/* <Route exact path="/profile" element={<Profile />} /> */}
+          <Route exact path="/profile" element={<Profile />} />
           <Route exact path="/dropzone" element={<Dropzone />} />
           <Route exact path="/createUser" element={<AuthForm />} />
           <Route exact path="/signUp" element={<SignUp />} />
